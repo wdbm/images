b9ca2f62d1b86c77f8080f88b66ab47635610d7c
Will Breaden Madden <wbm@protonmail.ch>
2019-02-07 17:41:11 +0000

    add ggF, VH, VBF and ttH Feynman diagrams

---
+++ LaTeX/Feynman_diagrams/compile.sh
@@ -0,0 +1,22 @@
+rm feyngraph.*
+pdflatex ggF.tex
+mpost feyngraph.mp
+pdflatex ggF.tex
+
+rm feyngraph.*
+pdflatex VH.tex
+mpost feyngraph.mp
+pdflatex VH.tex
+
+rm feyngraph.*
+pdflatex VBF.tex
+mpost feyngraph.mp
+pdflatex VBF.tex
+
+rm feyngraph.*
+pdflatex ttH.tex
+mpost feyngraph.mp
+pdflatex ttH.tex
+
+rm feyngraph.*
+rm *.aux *.dvi *.log
